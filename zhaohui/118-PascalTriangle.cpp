9a3b25cf416fe447b5a2c142045a6abeba979bab
wuzhaohui <thwuzhaohui@163.com>
2015-08-14 23:07:59 +0800

    solve 119,4

--- zhaohui/118-PascalTriangle.cpp
+++ zhaohui/118-PascalTriangle.cpp
@@ -35,6 +35,21 @@ public:
 		pascal.push_back(nLine);
 		return pascal;
 	}
+	vector<int> getRow(int rowIndex) {
+        if(rowIndex ==0){
+			vector<int> row(1,1);
+			return row;
+		}
+		vector<int> pRow = getRow(rowIndex-1);
+		int temp = pRow[0],temp1=0;
+		for(int i=1;i<rowIndex;i++){
+			temp1 = pRow[i];
+			pRow[i] = temp + pRow[i];
+			temp = temp1;
+		}
+		pRow.push_back(1);
+		return pRow;	
+    }
 };
 
 int main() {
@@ -46,4 +61,8 @@ int main() {
 		}
 		cout << endl;
 	}
+	vector<int> row = solution.getRow(3);
+	for(int i=0;i<row.size();i++)
+		cout<<row[i]<<" ";
+	cout<<endl;
 }
