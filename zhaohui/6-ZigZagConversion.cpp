00fa0cf6698c9b6bb9b6cbaaab668296d46d1ea5
wuzhaohui <thwuzhaohui@163.com>
2015-08-15 19:58:19 +0800

    solve 8,9,12,13

--- zhaohui/6-ZigZagConversion.cpp
+++ zhaohui/6-ZigZagConversion.cpp
@@ -5,11 +5,16 @@ using std::string;
 class Solution {
 public:
 	string convert(string s, int numRows) {
+		if (numRows == 1)
+			return s;
 		int  len = s.size();
 		string sZig = "";
 		int index = 0;
 		//int i, n;
-		for (int i = 0; i < numRows-1; i++) {
+		for (int n = 0; (index = (2 * n )*(numRows - 1))<len; n++) {
+			sZig = sZig + s[index];
+		}
+		for (int i = 1; i < numRows - 1; i++) {
 			for (int n = 0; (index = 2 * n*(numRows - 1) - i) < len; n++) {
 				if (index >= 0)
 					sZig = sZig + s[index];
@@ -17,9 +22,12 @@ public:
 					sZig = sZig + s[index];
 			}
 		}
-		for (int n = 0; (index = (2 * n + 1)*(numRows - 1))<len; n++) {
-			sZig = sZig + s[index];
+		if (numRows - 1 > 0) {
+			for (int n = 0; (index = (2 * n + 1)*(numRows - 1))<len; n++) {
+				sZig = sZig + s[index];
+			}
 		}
+		return sZig;
 	}
 };
 
@@ -30,5 +38,6 @@ int main()
 	int numRows = 3;
 	Solution solution;
 	cout << solution.convert(s, numRows) << std::endl;
-    return 0;
+	cout << solution.convert("a", 1) << std::endl;
+	return 0;
 }
